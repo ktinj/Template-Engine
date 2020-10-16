65dbc554a1b914049e9899af6b7e95ab42b93433
Kristin Jacobsen <kristinjacobsenn@gmail.com>
2020-10-16 15:39:08 -0500

    updated app

--- Develop/app.js
+++ Develop/app.js
@@ -13,26 +13,218 @@ const render = require("./lib/htmlRenderer");
 const teamMembers = [];
 const idArray = [];
 
+// creating manager
 function appMenu() {
     fuction createManager() {
-        console.log("Build your team!");
         inquirer.prompt([
             {
                 type: "input",
                 name: "managerName",
                 message: "What is the manager's name?",
-                validate: anser => {
+                validate: answer => {
                     if (answer !== "") {
                         return true;
                     }         
                 return "Please enter a name!";
-                   }
+                }
+            },
+            type: "input",
+            name: "managerID",
+            message: "What is the manager's ID?",
+            validate: answer => {
+                const pass = answer.match(
+                    /^[1-9]\d*$/
+                );
+                if (pass) {
+                    return true;
+                }
+                return "Enter a number greater than 0";
+                }
+            },
+            type: "input",
+            name: "managerEmail",
+            message: "What is the manager's email?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }         
+            return "Please enter an email!";
+                }
+            },
+            type: "input",
+            name: "officeNumber",
+            message: "What is the manager's office number?",
+            validate: answer => {
+                const pass = answer.match(
+                    /^[1-9]\d*$/
+                );
+                if (pass) {
+                    return true;
+                }
+                return "Enter a number greater than 0";
+                }
             }
-        ])
+        ]).then(answers => {
+            const manager = new Manager(answers.managerMame, answers.managerID, answers.managerEmail, answers.officeNumber);
+            teamMembers.push(manager);
+            idArray.push(answers.managerID);
+            createTeam();
+        }
+    };
+
+    // creating team function
+    function createTeam() {
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "memberChoice",
+                message: "Which type of team member would you like to add?",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "I don't want to add any more team members"
+                ]
+            }
+        ]).then(userChoice => {
+            switch(userChoice.memberChoice) {
+                case "Engineer":
+                    addEngineer();
+                    break;
+                case "Intern":
+                    addIntern();
+                    break;
+                default:
+                    buildTeam();
+            }
+        });
     }
 
+    // adding engineer
+    fuction addEngineer() {
+        inquirer.prompt([
+            {
+            type: "input",
+            name: "engineerName",
+            message: "What is the engineer's name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }         
+                return "Please enter a name!";
+                }
+            },
+            {
+            type: "input",
+            name: "engineerID",
+            message: "What is the engineer's ID?",
+                validate: answer => {
+                const pass = answer.match(
+                    /^[1-9]\d*$/
+                );
+                if (pass) {
+                    return true;
+                }
+                return "Enter a number greater than 0";
+                }
+            },
+            {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is the engineer's email?",
+                validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }         
+            return "Please enter an email!";
+                }
+            },
+            type: "input",
+            name: "engineerGithub",
+            message: "What is the engineer's GitHub?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+            return "Please enter GitHub username";
+                }
+            }
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerMame, answers.engineerID, answers.engineerEmail, answers.engineerGithub);
+            teamMembers.push(engineer);
+            idArray.push(answers.engineerID);
+            createTeam();
+        }
+    };
 
+    // adding intern
+    fuction addIntern() {
+        inquirer.prompt([
+            {
+            type: "input",
+            name: "internName",
+            message: "What is the intern's name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }         
+                return "Please enter a name!";
+                }
+            },
+            {
+            type: "input",
+            name: "internID",
+            message: "What is the interns ID?",
+                validate: answer => {
+                    const pass = answer.match(
+                        /^[1-9]\d*$/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Enter a number greater than 0";
+                    }
+                },
+                {
+            type: "input",
+            name: "internEmail",
+            message: "What is the interns email?",
+                    validate: answer => {
+                        if (answer !== "") {
+                            return true;
+                        }         
+                    return "Please enter an email!";
+                        }
+                    },
+                    {
+            type: "input",
+            name: "internSchool",
+            message: "What is the school email?",
+                    validate: answer => {
+                        if (answer !== "") {
+                            return true;
+                        }         
+                    return "Please enter an school name!";
+                        }
+                    }
+        ]).then(answers => {
+            const intern = new Intern(answers.internMame, answers.internID, answers.internEmail, answers.internSchool);
+            teamMembers.push(intern);
+            idArray.push(answers.internID);
+            createTeam();
+                });
+            }
+
+
+
+function buildTeam() {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
 }
+
+
+        
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
